e6f3d9813e872996302b110e4a68ed85e6fc098b
unknown <danilo25aciole@gmail.com>
2022-12-06 15:52:55 -0300

    Ajustes

---
+++ pds/src/CreateEvent.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './styles/createEvent.css';
+
+function CreateEvent(){
+    return(
+        <div className='Barra'>
+            <div className='BarraCima'>
+                <text className='txt1'> Comece a vender seus ingressos</text>
+                <text className='txt2'> Crie o evento do seu jeito</text>
+            </div>
+        </div>
+    )
+}
+
+export default CreateEvent
